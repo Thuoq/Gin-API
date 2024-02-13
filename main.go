cd96e9a97dd9dfa2837ff37aa2634bea215a7d80
Thuoq <thuonghamno3xz@gmail.com>
2024-02-13 16:12:06 +0700

    :+1: Update fake database connection

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
 	"net/http"
 	"time"
 )
@@ -18,10 +19,10 @@ type TodoItem struct {
 }
 
 func main() {
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:thuong123@tcp(127.0.0.1:3306)/gin-api?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-
+		log.Fatal(err)
 	}
 	//now := time.Now().UTC()
 	//
