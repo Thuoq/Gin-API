719a9ec27f30e01515f8a493b6932665fbde9178
Thuoq <thuonghamno3xz@gmail.com>
2024-02-13 16:09:14 +0700

    :+1: First commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"net/http"
+	"time"
+)
+
+type TodoItem struct {
+	Id          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Status      string     `json:"status"`
+	CreatedAt   *time.Time `json:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at"`
+}
+
+func main() {
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+
+	}
+	//now := time.Now().UTC()
+	//
+	//item := TodoItem{
+	//	Id:          1,
+	//	Title:       "Task 1",
+	//	Description: "Content 1",
+	//	Status:      "Doing",
+	//	CreatedAt:   &now,
+	//	UpdatedAt:   nil,
+	//}
+	//jsData, err := json.Marshal(item)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//log.Println(string(jsData))
+	//
+	//jsonString := "{\"id\":1,\"title\":\"Task 1\",\"description\":\"Content 1\",\"status\":\"Doing\",\"created_at\":\"2024-02-13T07:44:00.669384Z\",\"updated_at\":null}"
+	//
+	//var item2 TodoItem
+	//if err := json.Unmarshal([]byte(jsonString), &item2); err != nil {
+	//	log.Println(err)
+	//}
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	v1 := r.Group("/v1")
+	{
+		items := v1.Group("/items")
+		{
+			items.GET("")
+			items.POST("")
+			items.DELETE("/:id")
+			items.GET("/:id")
+			items.PATCH("/:id")
+		}
+	}
+	r.Run(":3000")
+}
